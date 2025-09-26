ec0a3ef1db93fd7b876814b1ea522ad198ddf92f
Pramod <pramodshenkar2210@gmail.com>
2025-09-27 00:27:39 +0530

    Org CRD structs written

--- operator/api/v1/org_types.go
+++ operator/api/v1/org_types.go
@@ -20,42 +20,20 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // OrgSpec defines the desired state of Org
 type OrgSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-	// The following markers will use OpenAPI v3 schema to validate the value
-	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
-
-	// foo is an example field of Org. Edit org_types.go to remove/update
-	// +optional
-	Foo *string `json:"foo,omitempty"`
+	IndentConsumerCount int `json:"IndentConsumerCount,omitempty"`
 }
 
 // OrgStatus defines the observed state of Org.
 type OrgStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// For Kubernetes API conventions, see:
-	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
-
-	// conditions represent the current state of the Org resource.
-	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
-	//
-	// Standard condition types include:
-	// - "Available": the resource is fully functional
-	// - "Progressing": the resource is being created or updated
-	// - "Degraded": the resource failed to reach or maintain its desired state
-	//
 	// The status of each condition is one of True, False, or Unknown.
 	// +listType=map
 	// +listMapKey=type
 	// +optional
 	Conditions []metav1.Condition `json:"conditions,omitempty"`
+
+	RunningConsumerCount int `json:"RunningConsumerCount,omitempty"`
 }
 
 // +kubebuilder:object:root=true
